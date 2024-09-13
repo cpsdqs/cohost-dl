ce2ee4b9d0693595b02276b87f00cc4e087b1002
cpsdqs <cpsdqs@cloudwithlightning.net>
2024-09-13 21:24:19 +0200

    add missing file!!

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import * as data from "../CONFIG.ts";
+
+export const COOKIE = data.COOKIE;
+
+export const POSTS = data.POSTS ?? [];
+
+export const PROJECTS = data.PROJECTS ?? [];
+
+export const SKIP_POSTS = data.SKIP_POSTS ?? [];
+
+export const DATA_PORTABILITY_ARCHIVE_PATH =
+    data.DATA_PORTABILITY_ARCHIVE_PATH ?? "";
+
+export const DO_NOT_FETCH_HOSTNAMES = data.DO_NOT_FETCH_HOSTNAMES ?? [];
+
+export const ENABLE_JAVASCRIPT = data.ENABLE_JAVASCRIPT ?? false;
